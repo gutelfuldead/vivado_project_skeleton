4e50eeb9c1409aa00c781c43d6d935040bb67140
jgutel <gutel@hcs.ufl.edu>
2019-01-16 15:50:20 -0800

    updated build_bitstream

--- src/tcl/build_bitstream.tcl
+++ src/tcl/build_bitstream.tcl
@@ -1,18 +1,18 @@
-set projName [lindex $argv 0]
-set outputFolder [lindex $argv 1]
-puts "$projName chosen"
+set design_name [lindex $argv 0]
+set output_dir [lindex $argv 1]
+puts "$design_name chosen"
 
-open_project ./$projName/$projName.xpr
+open_project ./$design_name/$design_name.xpr
 
 launch_runs synth_1
 wait_on_run synth_1
 open_run synth_1
-report_timing_summary -file $outputFolder/$projName.timing_synth.log
-report_utilization -file $outputFolder/$projName.utilization_synth.log
+report_timing_summary -file $output_dir/$design_name.timing_synth.log
+report_utilization -file $output_dir/$design_name.utilization_synth.log
 
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 open_run impl_1
-report_timing_summary -file $outputFolder/$projName.timing_impl.log
-report_utilization -file $outputFolder/$projName.utilization_impl.log
+report_timing_summary -file $output_dir/$design_name.timing_impl.log
+report_utilization -file $output_dir/$design_name.utilization_impl.log
 
