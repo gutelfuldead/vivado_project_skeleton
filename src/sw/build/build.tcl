4965ba5a2af6e50d43751f27c0df9fe3927b357f
jgutel <gutel@hcs.ufl.edu>
2019-07-23 17:56:14 -0700

    renamed projects

--- src/sw/build/build.tcl
+++ src/sw/build/build.tcl
@@ -6,22 +6,22 @@ set projNameBit $projName$bitString
 setws .
 
 # import the hdf
-createhw -name hw1 -hwspec ../../outputFiles/$projName.hdf
+createhw -name hw -hwspec ../../outputFiles/$projName.hdf
 
 # create bsp
-createbsp -name bsp1 -hwproject hw1 -proc ps7_cortexa9_0
+createbsp -name bsp -hwproject hw -proc ps7_cortexa9_0
 
 # create fsbl
-createapp -name fsbl1 -app {Zynq FSBL} -hwproject hw1 -proc ps7_cortexa9_0
+createapp -name fsbl -app {Zynq FSBL} -hwproject hw -proc ps7_cortexa9_0
 
 # create empty app
-createapp -name app1 -app {Empty Application} -bsp bsp1 -hwproject hw1 -proc ps7_cortexa9_0
-importsources -name app1 -path ../src -linker-script   
+createapp -name app -app {Empty Application} -bsp bsp -hwproject hw -proc ps7_cortexa9_0
+importsources -name app -path ../src -linker-script   
 
 # add compiler symbols
-# configapp -app app1 define-compiler-symbols CUSTOM_SYMBOL
-configapp -app app1 linker-script ../src/lscript.ld
-configapp -app app1 include-path ../src/some-folder
+# configapp -app app define-compiler-symbols CUSTOM_SYMBOL
+configapp -app app linker-script ../src/lscript.ld
+configapp -app app include-path ../src/some-folder
 
 # build the project
 projects -build
--- src/sw/build/load.tcl
+++ src/sw/build/load.tcl
@@ -7,16 +7,16 @@ setws .
 # connect to board
 # connect -host remotehost.com -port 3121
 connect -host localhost -port 3121
-source ./hw1/ps7_init.tcl
+source ./hw/ps7_init.tcl
 targets -set -nocase -filter {name =~"APU*"} -index 0
 rst -system
 after 3000
-fpga -file ./hw1/$projNameBit
+fpga -file ./hw/$projNameBit
 targets -set -nocase -filter {name =~"APU*"} -index 0
 ps7_init
 ps7_post_config
 targets -set -nocase -filter {name =~ "ARM*#0"} -index 0
-dow ./app1/Debug/app1.elf
+dow ./app/Debug/app.elf
 
 # run
 targets -set -nocase -filter {name =~ "ARM*#0"} -index 0
